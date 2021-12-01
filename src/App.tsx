70084eceb10ebd465793f7dbdf4a9c448ca297be
rowlandwilliams <rowland.williams.94@gmail.com>
2021-12-01 18:06:52 +0000

    add vs code config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Visualisation } from './components/Visualisation/Visualisation';
 
+const suh = 'dude';
+
+const dude = 'suh ' + dude;
 function App() {
     return <Visualisation />;
 }
