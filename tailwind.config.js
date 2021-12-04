5f8357671b93318efc39fbf6b8df099755a0ede6
rowlandwilliams <rowland.williams.94@gmail.com>
2021-12-04 17:14:40 +0000

    rm forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,23 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        "tt-interfaces-regular": ["TTInterfaces-Regular"],
-        "tt-interfaces-italic": ["TTInterfaces-Italic"],
-        "tt-interfaces-demi": ["TTInterfaces-Demi"],
-        "tt-interfaces-bold": ["TTInterfaces-Bold"],
-      },
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                'tt-interfaces-regular': ['TTInterfaces-Regular'],
+                'tt-interfaces-italic': ['TTInterfaces-Italic'],
+                'tt-interfaces-demi': ['TTInterfaces-Demi'],
+                'tt-interfaces-bold': ['TTInterfaces-Bold'],
+            },
+        },
+        colors: {
+            ...colors,
+            transparent: 'transparent',
+            current: 'currentColor',
+        },
     },
-    colors: {
-      ...colors,
-      transparent: "transparent",
-      current: "currentColor",
+    variants: {
+        extend: {},
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require("@tailwindcss/forms"), // import tailwind forms
-  ],
 };
